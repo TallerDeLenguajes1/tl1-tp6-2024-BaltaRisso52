2b11edd2a1a4bd6df8a140999dae9620995343b0
BaltaRisso52 <rissopatronivan62@gmail.com>
2024-05-14 12:45:30 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+
+Console.WriteLine("Ingrese el numero a invertir: ");
+int i = 0;
+string s = Console.ReadLine();
+int invertido = 0;
+int divisor = 1;
+
+if (int.TryParse(s, out i))
+{
+    if (i >= 0)
+    {
+        while (i > 0)
+        {
+
+            while (i / divisor > 10)
+            {
+                divisor *= 10;
+            }
+            invertido += (i % 10) * divisor;
+            divisor /= 10;
+            i /= 10;
+        }
+        Console.WriteLine(invertido);
+
+
+    }
+}
+
+
+
+
